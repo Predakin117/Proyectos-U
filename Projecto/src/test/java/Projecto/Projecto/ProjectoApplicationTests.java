e3ade0b4ed324b3fb51bfa71cc9330b9d4eff7f9
Predakin117 <130272665+Predakin117@users.noreply.github.com>
2025-10-19 18:46:50 -0500

    Add files via upload

---
+++ Projecto/src/test/java/Projecto/Projecto/ProjectoApplicationTests.java
@@ -0,0 +1,13 @@
+package Projecto.Projecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
